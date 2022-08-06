7d1aec8f7ec3ed9918d31c78e801cf0bdf8d31dd
edrush404 <j.cabral@aluno.senai.br>
2022-08-06 19:18:19 -0300

    atualizando exercicio python

--- exercicio.py
+++ exercicio.py
@@ -1,10 +1,12 @@
-n1 = int(input('digite um numero: '))
-print('analisando o valor {} seu antecessor é {} e seu sucessor é {}'.format(n1, (n1-1), (n1+1)))
+# n1 = int(input('digite um numero: '))
+# print(f'O dobro de {n1} é {n1*2}\nO triplo de {n1} é {n1*3}\nA raiz quadrada de {n1} é {n1**(1/2):.2f}')
 
-# nota1 = float(input('Digite sua primeira nota?' ))
-# nota2 = float(input('Digite sua segunda nota? '))
-# soma = (nota1 + nota2) /2
-# print('Você ficou com a média {} '.format(soma))
+# n1 = int(input('digite um numero: '))
+# print('analisando o valor {} seu antecessor é {} e seu sucessor é {}'.format(n1, (n1-1), (n1+1)))
+
+nota1 = float(input('Digite sua primeira nota? '))
+nota2 = float(input('Digite sua segunda nota? '))
+print(f'Você ficou com a média {(nota1+nota2)/2:.1f}')
 
 
 
