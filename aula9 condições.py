2fe7d8919a8d29ca4d8ac53d06d18b430559f73a
edrush404 <j.cabral@aluno.senai.br>
2022-08-15 13:10:22 -0300

    atualizando aula de condições

---
+++ aula9 condições.py
@@ -0,0 +1,17 @@
+# nome = str(input('Qual é o seu nome: '))
+# if nome == 'Gustavo':
+#     print('Olá!')
+# else:
+#     print('Seu nome é tão normal!')
+# print('Bom dia {}!'.format(nome))
+
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua segunda nota: '))
+result = (n1 + n2)/2
+print(f'Sua nota média foi {result}')
+if result >= 6:
+    print('Média boa, Parabéns!!')
+else:
+    print('Média ruim :(')
+
+
--- exercicio8  manipulando txt.py
+++ exercicio8  manipulando txt.py
@@ -1,7 +1,11 @@
-nome = str(input('Digite seu nome completo: ')).strip()
-print('Analisando seu nome.......')
-print(f'{nome.upper()}\n{nome.lower()}')
-print('{}'.format(len(nome)-nome.count(' ')))
-# print('{}'.format(nome.find(' '))) # em baixo de outra forma
-separar = nome.split()
-print('{}'.format(len(separar[0]))) # de outra forma
+# nome = str(input('Digite seu nome completo: ')).strip()
+# print('Analisando seu nome.......')
+# print(f'{nome.upper()}\n{nome.lower()}')
+# print('{}'.format(len(nome)-nome.count(' ')))
+# # print('{}'.format(nome.find(' '))) # em baixo de outra forma
+# separar = nome.split()
+# print('{}'.format(len(separar[0]))) # de outra forma
+
+nome = str(input('Digite seu nome completo: ')).strip().split()
+print(nome[0])
+print(nome[-1])
